ed143b9b4becd25bfbcb8c5ffaa17e5da0e912b6
DESKTOP-EJR3VAJ\Usuario <borjafruizdelgado@gmail.com>
2018-04-28 00:40:16 +0200

    New menu

--- LaraCroftGO/Assets/Scripts/MainMenu.cs
+++ LaraCroftGO/Assets/Scripts/MainMenu.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
+    
 
-	public void PlayGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
-        Debug.Log("Quiting the game");
         Application.Quit();
     }
 }
---
+++ LaraCroftGO/Assets/Scripts/MenuSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSound : MonoBehaviour {
+    public AudioSource audio;
+	// Use this for initialization
+	void Start () {
+
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+        audio.Play(44100);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
