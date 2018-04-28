f069b9ebf6dae38497103ee9b9e707af62d2e5af
DESKTOP-EJR3VAJ\Usuario <borjafruizdelgado@gmail.com>
2018-04-29 01:12:31 +0200

    lara fisicas y camara

---
+++ LaraCroftGO/Assets/Characters/Female Locomotion Pack/CameraController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offsetPos;
+    public float moveSpeed = 5;
+    public float turnSpeed = 5;
+    public float smoothSpeed = 0.5f;
+
+    Quaternion targetRotation;
+    Vector3 targetPos;
+
+    
+    bool smoothRotating = false;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        MoveWithTarget();
+        LookAtTarget();
+        if (Input.GetKeyDown(KeyCode.G) && !smoothRotating)
+        {
+            StartCoroutine("RotateArowndTarget", 45);
+        }
+        if (Input.GetKeyDown(KeyCode.H) && !smoothRotating)
+        {
+            StartCoroutine("RotateArowndTarget", -45);
+        }
+    }
+
+    void MoveWithTarget()
+    {
+        targetPos = target.position + offsetPos;
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+
+    void LookAtTarget()
+    {
+        targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
+
+    IEnumerator RotateArowndTarget(float angle)
+    {
+        Vector3 vel = Vector3.zero;
+        Vector3 targetOffsetPos = Quaternion.Euler(0, angle, 0) * offsetPos;
+        float distance = Vector3.Distance(offsetPos, targetOffsetPos);
+        smoothRotating = true;
+        while(distance > 0.02f)
+        {
+
+            offsetPos = Vector3.SmoothDamp(offsetPos, targetOffsetPos, ref vel, smoothSpeed);
+            distance = Vector3.Distance(offsetPos, targetOffsetPos);
+            yield return null;
+        }
+        smoothRotating = false;
+        offsetPos = targetOffsetPos;
+    }
+}
---
+++ LaraCroftGO/Assets/Characters/Female Locomotion Pack/PlayerController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float velocity = 5;
+    public float turnSpeed = 10;
+    Vector2 input;
+    float angle;
+    Quaternion targetRotation;
+    Transform cam;
+    Animator anim;
+    bool spacePressed;
+    public float height = 0.5f;
+    public float heightPadding = 0.05f;
+    public LayerMask ground;
+    public float maxGroundAngle = 120;
+    public bool debug;
+    float groundAngle;
+    Vector3 forward;
+    RaycastHit hitInfo;
+    bool grounded;
+    void Start()
+    {
+        cam = Camera.main.transform;
+        anim = GetComponent<Animator>();
+        spacePressed = false;
+    }
+
+    void Update()
+    {
+        GetInput();
+        CalculateDirection();
+        CalculateForward();
+        ClaculateGroundAngle();
+        CheckGround();
+        AplyGravity();
+        if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) return;
+        Rotate();
+        Move();
+        if (Input.GetKeyDown("space"))
+            anim.SetTrigger("space");
+        else anim.ResetTrigger("space");
+    }
+
+    private void AplyGravity()
+    {
+        if (!grounded)
+        {
+            transform.position += Physics.gravity * Time.deltaTime;
+        }
+    }
+
+    private void CheckGround()
+    {
+        if(Physics.Raycast(transform.position,-Vector3.up,out hitInfo, height + heightPadding, ground))
+        {
+            if (Vector3.Distance(transform.position, hitInfo.point) < height)
+            {
+                transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.up * height, 5 * Time.deltaTime);
+            }
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+
+    private void ClaculateGroundAngle()
+    {
+        if (!grounded)
+        {
+            groundAngle = 90;
+            return;
+        }
+        groundAngle = Vector3.Angle(hitInfo.normal, transform.forward);
+    }
+
+    private void CalculateForward()
+    {
+        if (!grounded)
+        {
+            forward = transform.forward;
+            return;
+        }
+        forward = Vector3.Cross(hitInfo.normal, -transform.right);
+    }
+
+    void GetInput()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+        anim.SetFloat("BlendX", input.x);
+        anim.SetFloat("BlendY", input.y);
+        
+    }
+
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+    }
+    
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,turnSpeed*Time.deltaTime);
+    }
+
+    void Move()
+    {
+        if (groundAngle >= maxGroundAngle) return;
+        transform.position += forward * velocity * Time.deltaTime;
+    }
+
+
+}
