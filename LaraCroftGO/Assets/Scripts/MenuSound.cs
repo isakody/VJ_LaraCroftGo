b448e5bb2c1f1effcda8984beed65f2d76b7a90d
DESKTOP-EJR3VAJ\Usuario <borjafruizdelgado@gmail.com>
2018-05-05 00:24:39 +0200

    Menu Principal

---
+++ LaraCroftGO/Assets/Scripts/MenuSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSound : MonoBehaviour {
+    public AudioSource sound;
+    public AudioClip audio;
+    public Slider volumeSlider;
+    public void OnClick()
+    {
+        sound.PlayOneShot(audio);
+    }
+
+}
+
